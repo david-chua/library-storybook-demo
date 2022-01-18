c1f2d92f14c13fcd2dfdcba711a9381ce307aca1
David Chua <dp_chua@hotmail.com>
2022-01-18 11:15:56 -0500

    prop fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './styles.module.css'
 import { Button } from './components/Button'
 import { DefaultPrimaryPillProps, SelectedPrimaryPillProps} from './props/TextOnlyPrimaryPillProps'
-import { PrimaryRectangleButton } from './props/TextOnlyPrimaryRectangleProps'
+import { PrimaryRectangleButton } from './props/TextOnly/Primary/TextOnlyPrimaryRectangleProps'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
